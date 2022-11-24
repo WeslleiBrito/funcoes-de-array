cf888be114990b622f6809fbe42582900c702424
WeslleiBrito <wesllei.wbs@gmail.com>
2022-11-24 14:47:21 -0300

    Exercico 1 incompleto

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,38 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+function changeToUpperCase(object) {
+    const newObject = {}
+
+    for (property in object) {
+        newObject[property] = object[property].toUpperCase()
+    }
+
+    return newObject
+}
+
+function turnTextToRunning(object) {
+    const newObject = {}
+    let text = ''
+
+    for (property in object) {
+
+        text = object[property]
+        console.log(text)
+        text = text.replace(`${text[0]}`, `${text[0].toUpperCase()}`)
+
+        for (index in text.substring(1)) {
+
+            if (65 <= text.charAt(index) <= 90 || 97 <= text.charAt(index) <= 122) {
+                text = text.substring(0, index) + ' ' + text.substring(index + 1)
+            } else {
+                text = text.replaceAll(`${text[index]}`, ' ')
+            }
+        }
+
+        console.log(text)
+    }
+}
+
+turnTextToRunning(objeto)
